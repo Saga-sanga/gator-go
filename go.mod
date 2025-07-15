445754446b338beeda8cc2d109dcc377a53fb84f
Saga-sanga <vulcan248@gmail.com>
2025-07-15 09:21:55 +0000

    refactor: add meainingful error names

--- go.mod
+++ go.mod
@@ -2,6 +2,7 @@ module github.com/saga-sanga/gator-go
 
 go 1.24.3
 
-require github.com/google/uuid v1.6.0
-
-require github.com/lib/pq v1.10.9
+require (
+	github.com/google/uuid v1.6.0
+	github.com/lib/pq v1.10.9
+)
--- handler_user.go
+++ handler_user.go
@@ -3,24 +3,61 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/saga-sanga/gator-go/internal/database"
 )
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.Arguments) != 1 {
+		return fmt.Errorf("usage: %s <name>", cmd.Name)
+	}
+	name := cmd.Arguments[0]
+
+	newUser := database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      name,
+	}
+
+	user, err := s.db.CreateUser(context.Background(), newUser)
+	if err != nil {
+		return fmt.Errorf("couldn't create user: %w", err)
+	}
+
+	err = s.cfg.SetUser(user.Name)
+	if err != nil {
+		return fmt.Errorf("couldn't set current user: %w", err)
+	}
+
+	fmt.Println("User created successfully!")
+	printUser(user)
+	return nil
+}
 
 func handlerLogin(s *state, cmd command) error {
 	if len(cmd.Arguments) != 1 {
 		return fmt.Errorf("usage: %s <name>", cmd.Name)
 	}
-
 	name := cmd.Arguments[0]
+
 	user, err := s.db.GetUser(context.Background(), name)
 	if err != nil {
-		return err
+		return fmt.Errorf("couldn't find user: %w", err)
 	}
 
 	err = s.cfg.SetUser(user.Name)
 	if err != nil {
-		return err
+		return fmt.Errorf("couldn't set current user: %w", err)
 	}
 
 	fmt.Println("User switched successfully!")
 	return nil
+}
+
+func printUser(user database.User) {
+	fmt.Printf(" * ID:       %v\n", user.ID)
+	fmt.Printf(" * Name:     %v\n", user.Name)
 }
