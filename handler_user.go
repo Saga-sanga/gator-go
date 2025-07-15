858caaf1b6c418b8d79a82fc2a43c637744dc538
Saga-sanga <vulcan248@gmail.com>
2025-07-15 10:23:57 +0000

    feat: add db reset command

--- handler_user.go
+++ handler_user.go
@@ -9,6 +9,20 @@ import (
 	"github.com/saga-sanga/gator-go/internal/database"
 )
 
+func handlerReset(s *state, cmd command) error {
+	if len(cmd.Arguments) != 0 {
+		return fmt.Errorf("usage: %s", cmd.Name)
+	}
+
+	err := s.db.DeleteUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("cannot delete users: %w", err)
+	}
+
+	fmt.Println("Users deleted successfully!")
+	return nil
+}
+
 func handlerRegister(s *state, cmd command) error {
 	if len(cmd.Arguments) != 1 {
 		return fmt.Errorf("usage: %s <name>", cmd.Name)
