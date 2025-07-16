d26fd37fe81df3dcd20c84d2e6c875295e5ee1b3
Saga-sanga <vulcan248@gmail.com>
2025-07-16 16:07:55 +0000

    feat: added addfeed handler

--- handler_agg.go
+++ handler_agg.go
@@ -3,6 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/saga-sanga/gator-go/internal/database"
 )
 
 func handlerAgg(s *state, cmd command) error {
@@ -12,4 +16,39 @@ func handlerAgg(s *state, cmd command) error {
 	}
 	fmt.Printf("%v\n", *feed)
 	return nil
+}
+
+func handlerAddFeed(s *state, cmd command) error {
+	ctx := context.Background()
+	if len(cmd.Arguments) != 2 {
+		return fmt.Errorf("usage: %s <name> <url>", cmd.Name)
+	}
+	name := cmd.Arguments[0]
+	url := cmd.Arguments[1]
+
+	user, err := s.db.GetUser(ctx, s.cfg.CurrentUserName)
+	if err != nil {
+		return fmt.Errorf("cannot retrieve current user: %w", err)
+	}
+
+	feed, err := s.db.CreateFeed(ctx, database.CreateFeedParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      name,
+		Url:       url,
+		UserID:    user.ID,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to create feed: %w", err)
+	}
+
+	printFeed(feed)
+	return nil
+}
+
+func printFeed(feed database.Feed) {
+	fmt.Printf(" * ID:       %v\n", feed.ID)
+	fmt.Printf(" * Name:     %v\n", feed.Name)
+	fmt.Printf(" * URL:     %v\n", feed.Url)
 }
